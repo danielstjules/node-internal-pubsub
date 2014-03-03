ec308d1d272ed9c79e1aaf7b9ec668fcd41ab9cc
Daniel St. Jules <danielst.jules@gmail.com>
2014-03-03 00:26:20 -0500

    Fix handling of duplicate channels in subscribe

--- spec/subscriberSpec.js
+++ spec/subscriberSpec.js
@@ -36,59 +36,61 @@ describe('Subscriber', function() {
   });
 
   describe('subscribe', function() {
-    it('subscribes to a given channel', function() {
-      var passedChannel;
-      var passedMsg;
+    var channels, messages, counts;
 
+    var messageListener = function(channel, message) {
+      channels.push(channel);
+      messages.push(message);
+    };
+
+    var subscribeListener = function(channel, count) {
+      channels.push(channel);
+      counts.push(count);
+    };
+
+    beforeEach(function() {
+      channels = [];
+      messages = [];
+      counts = [];
+    });
+
+    it('subscribes to a given channel', function() {
       sub.subscribe('testChannel');
-      sub.on('message', function(channel, msg) {
-        passedChannel = channel;
-        passedMsg = msg;
-      });
+      sub.on('message', messageListener);
 
       emitter.emit('testChannel', 'testMessage');
 
-      expect(passedChannel).to.be('testChannel');
-      expect(passedMsg).to.be('testMessage');
+      expect(channels[0]).to.be('testChannel');
+      expect(messages[0]).to.be('testMessage');
     });
 
     it('can subscribe to multiple channels', function() {
-      var passedChannels = [];
-      var passedMsgs = [];
-
       sub.subscribe('testChannel1');
       sub.subscribe('testChannel2');
-      sub.on('message', function(channel, msg) {
-        passedChannels.push(channel);
-        passedMsgs.push(msg);
-      });
+      sub.on('message', messageListener);
 
       emitter.emit('testChannel1', 'testMessage1');
       emitter.emit('testChannel2', 'testMessage2');
 
-      expect(passedChannels[0]).to.be('testChannel1');
-      expect(passedMsgs[0]).to.be('testMessage1');
-      expect(passedChannels[1]).to.be('testChannel2');
-      expect(passedMsgs[1]).to.be('testMessage2');
+      expect(channels[0]).to.be('testChannel1');
+      expect(messages[0]).to.be('testMessage1');
+      expect(channels[1]).to.be('testChannel2');
+      expect(messages[1]).to.be('testMessage2');
     });
 
     it('accepts multiple channels to subscribe to', function() {
-      var passedChannels = [];
-      var passedMsgs = [];
-
-      sub.subscribe('testChannel1', 'testChannel2');
-      sub.on('message', function(channel, msg) {
-        passedChannels.push(channel);
-        passedMsgs.push(msg);
-      });
+      sub.subscribe('testChannel1', 'testChannel2', 'testChannel3');
+      sub.on('message', messageListener);
 
       emitter.emit('testChannel1', 'testMessage1');
       emitter.emit('testChannel2', 'testMessage2');
+      emitter.emit('testChannel3', 'testMessage3');
 
-      expect(passedChannels[0]).to.be('testChannel1');
-      expect(passedMsgs[0]).to.be('testMessage1');
-      expect(passedChannels[1]).to.be('testChannel2');
-      expect(passedMsgs[1]).to.be('testMessage2');
+      [1, 2, 3].forEach(function(i) {
+        var key = i - 1;
+        expect(channels[key]).to.be('testChannel' + i);
+        expect(messages[key]).to.be('testMessage' + i);
+      });
     });
 
     it('increments the subscription count', function() {
@@ -96,6 +98,39 @@ describe('Subscriber', function() {
       sub.subscribe('testChannel1', 'testChannel2');
 
       expect(sub.count).to.be(previousCount + 2);
+    });
+
+    it('emits a subscribe event for each, with the channel and count', function() {
+      sub.on('subscribe', subscribeListener);
+      sub.subscribe('testChannel1', 'testChannel2');
+
+      expect(channels.length).to.be(2);
+      expect(channels[0]).to.be('testChannel1');
+      expect(counts[0]).to.be(1);
+      expect(channels[1]).to.be('testChannel2');
+      expect(counts[1]).to.be(2);
+    });
+
+    it('ignores channels to which it is already subscribed', function() {
+      sub.on('subscribe', subscribeListener);
+      sub.subscribe('testChannel1', 'testChannel1', 'testChannel2');
+
+      expect(channels.length).to.be(2);
+      expect(channels[0]).to.be('testChannel1');
+      expect(counts[0]).to.be(1);
+      expect(channels[1]).to.be('testChannel2');
+      expect(counts[1]).to.be(2);
+    });
+
+    it("does not subscribe to EventEmitter's newListener/removeListener", function() {
+      sub.on('message', messageListener);
+      sub.subscribe('newListener');
+      sub.subscribe('testChannel');
+      emitter.emit('newListener', 'testMessage');
+
+      expect(channels.length).to.be(1);
+      expect(channels[0]).to.be('newListener');
+      expect(messages[0]).to.be('testMessage');
     });
   });
 });
