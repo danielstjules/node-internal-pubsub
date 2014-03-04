f0ecd6b886ef29f6130533501a8dfedfa7d4c5ff
Daniel St. Jules <danielst.jules@gmail.com>
2014-03-03 22:39:00 -0500

    Fixed punsubscribe event, added additional Subscriber specs

--- spec/subscriberSpec.js
+++ spec/subscriberSpec.js
@@ -4,9 +4,15 @@ var PatternEmitter = require('pattern-emitter');
 var Subscriber     = require('../lib/subscriber');
 
 describe('Subscriber', function() {
-  var emitter, sub, channels, messages, counts;
+  var emitter, sub, channels, patterns, messages, counts;
 
   var messageListener = function(channel, message) {
+    channels.push(channel);
+    messages.push(message);
+  };
+
+  var pmessageListener = function(pattern, channel, message) {
+    patterns.push(pattern);
     channels.push(channel);
     messages.push(message);
   };
@@ -16,6 +22,7 @@ describe('Subscriber', function() {
     sub = new Subscriber(emitter);
 
     channels = [];
+    patterns = [];
     messages = [];
     counts = [];
   });
@@ -67,10 +74,10 @@ describe('Subscriber', function() {
       emitter.emit('testChannel1', 'testMessage1');
       emitter.emit('testChannel2', 'testMessage2');
 
-      expect(channels[0]).to.be('testChannel1');
-      expect(messages[0]).to.be('testMessage1');
-      expect(channels[1]).to.be('testChannel2');
-      expect(messages[1]).to.be('testMessage2');
+      expect(channels).to.have.length(2);
+      expect(channels).to.contain('testChannel1', 'testChannel2');
+      expect(messages).to.have.length(2);
+      expect(messages).to.contain('testMessage1', 'testMessage2');
     });
 
     it('accepts multiple channels to subscribe to', function() {
@@ -81,11 +88,10 @@ describe('Subscriber', function() {
       emitter.emit('testChannel2', 'testMessage2');
       emitter.emit('testChannel3', 'testMessage3');
 
-      [1, 2, 3].forEach(function(i) {
-        var key = i - 1;
-        expect(channels[key]).to.be('testChannel' + i);
-        expect(messages[key]).to.be('testMessage' + i);
-      });
+      expect(channels).to.have.length(3);
+      expect(channels).to.contain('testChannel1', 'testChannel2', 'testChannel3');
+      expect(messages).to.have.length(3);
+      expect(messages).to.contain('testMessage1', 'testMessage2', 'testMessage3');
     });
 
     it('increments the subscription count', function() {
@@ -99,22 +105,16 @@ describe('Subscriber', function() {
       sub.on('subscribe', subscribeListener);
       sub.subscribe('testChannel1', 'testChannel2');
 
-      expect(channels.length).to.be(2);
-      expect(channels[0]).to.be('testChannel1');
-      expect(counts[0]).to.be(1);
-      expect(channels[1]).to.be('testChannel2');
-      expect(counts[1]).to.be(2);
+      expect(channels).to.eql(['testChannel1', 'testChannel2']);
+      expect(counts).to.eql([1, 2]);
     });
 
     it('ignores channels to which it is already subscribed', function() {
       sub.on('subscribe', subscribeListener);
       sub.subscribe('testChannel1', 'testChannel1', 'testChannel2');
 
-      expect(channels.length).to.be(2);
-      expect(channels[0]).to.be('testChannel1');
-      expect(counts[0]).to.be(1);
-      expect(channels[1]).to.be('testChannel2');
-      expect(counts[1]).to.be(2);
+      expect(channels).to.eql(['testChannel1', 'testChannel2']);
+      expect(counts).to.eql([1, 2]);
     });
 
     it("does not subscribe to EventEmitter's newListener/removeListener", function() {
@@ -123,9 +123,8 @@ describe('Subscriber', function() {
       sub.subscribe('testChannel');
       emitter.emit('newListener', 'testMessage');
 
-      expect(channels.length).to.be(1);
-      expect(channels[0]).to.be('newListener');
-      expect(messages[0]).to.be('testMessage');
+      expect(channels).to.eql(['newListener']);
+      expect(messages).to.eql(['testMessage']);
     });
   });
 
@@ -146,8 +145,8 @@ describe('Subscriber', function() {
       emitter.emit('testChannel1', 'testMessage1');
       emitter.emit('testChannel2', 'testMessage2');
 
-      expect(channels[0]).to.be('testChannel2');
-      expect(messages[0]).to.be('testMessage2');
+      expect(channels).to.eql(['testChannel2']);
+      expect(messages).to.eql(['testMessage2']);
     });
 
     it('can unsubscribe from multiple channels', function() {
@@ -159,9 +158,8 @@ describe('Subscriber', function() {
         emitter.emit('testChannel' + i, 'testMessage' + i);
       });
 
-      expect(channels.length).to.be(1);
-      expect(channels[0]).to.be('testChannel3');
-      expect(messages[0]).to.be('testMessage3');
+      expect(channels).to.eql(['testChannel3']);
+      expect(messages).to.eql(['testMessage3']);
     });
 
     it('accepts multiple channels to unsubscribe from', function() {
@@ -172,9 +170,8 @@ describe('Subscriber', function() {
         emitter.emit('testChannel' + i, 'testMessage' + i);
       });
 
-      expect(channels.length).to.be(1);
-      expect(channels[0]).to.be('testChannel3');
-      expect(messages[0]).to.be('testMessage3');
+      expect(channels).to.eql(['testChannel3']);
+      expect(messages).to.eql(['testMessage3']);
     });
 
     it('decrements the subscription count', function() {
@@ -188,22 +185,163 @@ describe('Subscriber', function() {
       sub.on('unsubscribe', unsubscribeListener);
       sub.unsubscribe('testChannel1', 'testChannel2');
 
-      expect(channels.length).to.be(2);
-      expect(channels[0]).to.be('testChannel1');
-      expect(counts[0]).to.be(2);
-      expect(channels[1]).to.be('testChannel2');
-      expect(counts[1]).to.be(1);
+      expect(channels).to.eql(['testChannel1', 'testChannel2']);
+      expect(counts).to.eql([2, 1]);
     });
 
     it('ignores channels not among its subscriptions', function() {
       sub.on('unsubscribe', unsubscribeListener);
       sub.unsubscribe('testChannel1', 'testChannel1', 'testChannel2');
 
-      expect(channels.length).to.be(2);
-      expect(channels[0]).to.be('testChannel1');
-      expect(counts[0]).to.be(2);
-      expect(channels[1]).to.be('testChannel2');
-      expect(counts[1]).to.be(1);
+      expect(channels).to.eql(['testChannel1', 'testChannel2']);
+      expect(counts).to.eql([2, 1]);
+    });
+  });
+
+  describe('psubscribe', function() {
+    var psubscribeListener = function(pattern, count) {
+      patterns.push(pattern);
+      counts.push(count);
+    };
+
+    it('subscribes to a given pattern', function() {
+      sub.psubscribe(/test/);
+      sub.on('pmessage', pmessageListener);
+
+      emitter.emit('testChannel', 'testMessage');
+
+      expect(patterns).to.eql([/test/]);
+      expect(channels).to.eql(['testChannel']);
+      expect(messages).to.eql(['testMessage']);
+    });
+
+    it('can subscribe to multiple patterns', function() {
+      sub.psubscribe(/test/);
+      sub.psubscribe(/^other/);
+      sub.on('pmessage', pmessageListener);
+
+      emitter.emit('testChannel', 'testMessage');
+      emitter.emit('otherChannel', 'otherMessage');
+
+      expect(patterns).to.eql([/test/, /^other/]);
+      expect(channels).to.eql(['testChannel', 'otherChannel']);
+      expect(messages).to.eql(['testMessage', 'otherMessage']);
+    });
+
+    it('accepts multiple patterns to subscribe to', function() {
+      sub.psubscribe(/test/, /^other/, /TestChannel/i);
+      sub.on('pmessage', pmessageListener);
+
+      emitter.emit('testChannel', 'testMessage');
+      emitter.emit('otherChannel', 'otherMessage');
+
+      expect(patterns).to.eql([/test/, /TestChannel/i, /^other/]);
+      expect(channels).to.eql(['testChannel', 'testChannel', 'otherChannel']);
+      expect(messages).to.eql(['testMessage', 'testMessage', 'otherMessage']);
+    });
+
+    it('increments the subscription count', function() {
+      var previousCount = sub.count;
+      sub.psubscribe(/test/, /^other/);
+
+      expect(sub.count).to.be(previousCount + 2);
+    });
+
+    it('emits a subscribe event for each, with the pattern and count', function() {
+      sub.on('psubscribe', psubscribeListener);
+      sub.psubscribe(/test/, /^other/);
+
+      expect(patterns).to.eql([/test/, /^other/]);
+      expect(counts).to.eql([1, 2]);
+    });
+
+    it('ignores patterns to which it is already subscribed', function() {
+      sub.on('psubscribe', psubscribeListener);
+      sub.psubscribe(/test/, /test/, /^other/);
+
+      expect(patterns).to.eql([/test/, /^other/]);
+      expect(counts).to.eql([1, 2]);
+    });
+
+    it("does not subscribe to EventEmitter's newListener/removeListener", function() {
+      sub.on('pmessage', pmessageListener);
+      sub.psubscribe(/newListener/);
+      sub.psubscribe(/test/);
+      emitter.emit('newListener', 'testMessage');
+
+      expect(channels).to.eql(['newListener']);
+      expect(messages).to.eql(['testMessage']);
+    });
+  });
+
+  describe('punsubscribe', function() {
+    var punsubscribeListener = function(pattern, count) {
+      patterns.push(pattern);
+      counts.push(count);
+    };
+
+    beforeEach(function() {
+      sub.psubscribe(/test/, /TestChannel/i, /^other/);
+    });
+
+    it('unsubscribes from a given pattern', function() {
+      sub.on('pmessage', pmessageListener);
+      sub.punsubscribe(/test/);
+
+      emitter.emit('testChannel', 'testMessage1');
+      emitter.emit('testing', 'testMessage2');
+
+      expect(patterns).to.eql([/TestChannel/i]);
+      expect(channels).to.eql(['testChannel']);
+      expect(messages).to.eql(['testMessage1']);
+    });
+
+    it('can unsubscribe from multiple patterns', function() {
+      sub.punsubscribe(/test/);
+      sub.punsubscribe(/TestChannel/i);
+      sub.on('pmessage', pmessageListener);
+
+      emitter.emit('testChannel', 'testMessage');
+      emitter.emit('otherChannel', 'otherMessage');
+
+      expect(patterns).to.eql([/^other/]);
+      expect(channels).to.eql(['otherChannel']);
+      expect(messages).to.eql(['otherMessage']);
+    });
+
+    it('accepts multiple patterns to unsubscribe from', function() {
+      sub.punsubscribe(/test/, /TestChannel/i);
+      sub.on('pmessage', pmessageListener);
+
+      emitter.emit('testChannel', 'testMessage');
+      emitter.emit('otherChannel', 'otherMessage');
+
+      expect(patterns).to.eql([/^other/]);
+      expect(channels).to.eql(['otherChannel']);
+      expect(messages).to.eql(['otherMessage']);
+    });
+
+    it('decrements the subscription count', function() {
+      var previousCount = sub.count;
+      sub.punsubscribe(/test/, /TestChannel/i);
+
+      expect(sub.count).to.be(previousCount - 2);
+    });
+
+    it('emits a punsubscribe event for each, with the pattern and count', function() {
+      sub.on('punsubscribe', punsubscribeListener);
+      sub.punsubscribe(/test/, /TestChannel/i);
+
+      expect(patterns).to.eql([/test/, /TestChannel/i]);
+      expect(counts).to.eql([2, 1]);
+    });
+
+    it('ignores patterns not among its subscriptions', function() {
+      sub.on('punsubscribe', punsubscribeListener);
+      sub.punsubscribe(/test/, /test/, /TestChannel/i);
+
+      expect(patterns).to.eql([/test/, /TestChannel/i]);
+      expect(counts).to.eql([2, 1]);
     });
   });
 });
